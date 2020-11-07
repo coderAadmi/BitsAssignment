87cd2a2e5510247dbc1a3c004cf42193151b6b89
coderAadmi <pradyumnupadhyay2@gmail.com>
2020-11-07 11:58:18 +0530

    Spring Boot App initialized

---
+++ src/test/java/com/bits/prady/MyWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bits.prady;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
